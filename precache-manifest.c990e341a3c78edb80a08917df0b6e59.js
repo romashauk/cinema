57c44a0a2fc15980d635a992432c52c5b04a740f
Roman Shauk <shauk60@gmail.com>
2019-03-03 18:08:12 +0200

    Updates

---
+++ precache-manifest.c990e341a3c78edb80a08917df0b6e59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "164c522fc74e037b5c1d7218547563bd",
+    "url": "/cinema/static/media/notFound.164c522f.jpg"
+  },
+  {
+    "revision": "4b22e9b5c57ff48e643da9b79fb9e72d",
+    "url": "/cinema/static/media/logo.4b22e9b5.png"
+  },
+  {
+    "revision": "1ceff12817777686012730900490ce6e",
+    "url": "/cinema/static/media/background-img.1ceff128.jpg"
+  },
+  {
+    "revision": "99b60e0412e8a1fd4cbd",
+    "url": "/cinema/static/js/runtime~main.99b60e04.js"
+  },
+  {
+    "revision": "79431d53a10cfb27c79d",
+    "url": "/cinema/static/js/main.79431d53.chunk.js"
+  },
+  {
+    "revision": "2b3ea16e700563ed02bb",
+    "url": "/cinema/static/js/2.2b3ea16e.chunk.js"
+  },
+  {
+    "revision": "79431d53a10cfb27c79d",
+    "url": "/cinema/static/css/main.ee56c54f.chunk.css"
+  },
+  {
+    "revision": "403b64b05f0609a1e8d0ff6b918c7275",
+    "url": "/cinema/index.html"
+  }
+];
